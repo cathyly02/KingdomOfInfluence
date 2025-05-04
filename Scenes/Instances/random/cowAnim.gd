4fd1066b671581a6add68d481412b196d1e10e98
Sage Tesoro <satewarco@gmail.com>
2025-05-04 05:09:45 -0700

    animal anim

---
+++ Scenes/Instances/random/cowAnim.gd
@@ -0,0 +1,17 @@
+extends Node3D
+
+@onready var anim = $AnimationPlayer
+var last_position = Vector3.ZERO
+var interactable = true
+
+func _ready():
+	last_position = global_position
+
+func _process(delta):
+	var current_xz = Vector2(global_position.x, global_position.z)
+	var last_xz = Vector2(last_position.x, last_position.z)
+
+	if current_xz != last_xz:
+		anim.play("walk")
+
+	last_position = global_position
