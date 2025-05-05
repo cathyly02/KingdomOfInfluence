05dfe8166fd320ba36719b613d71a0e2d015d294
Devin Jay San Nicolas <dds324@nau.edu>
2025-05-04 19:10:05 -0700

    IMPROVED KINGDOM AND KINDA FINISHED CHICKEN QUEST

--- Global.gd
+++ Global.gd
@@ -6,3 +6,7 @@ var current_npc: Node = null
 func startNPCQuest() -> void:
 	# start quest
 	current_npc.get_node("Quest").startQuest()
+
+func endNPCQuest() -> void:
+	# end npc quest
+	current_npc.get_node("Quest").endQuest()
